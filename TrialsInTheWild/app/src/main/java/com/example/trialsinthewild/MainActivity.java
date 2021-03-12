17dd4c376c42a71a91f876af72301ef5abfb8602
JiaYiChe <59781205+JiaYiChe@users.noreply.github.com>
2021-03-12 00:34:12 -0600

    navigation added

--- TrialsInTheWild/app/src/main/java/com/example/trialsinthewild/MainActivity.java
+++ TrialsInTheWild/app/src/main/java/com/example/trialsinthewild/MainActivity.java
@@ -1,18 +1,74 @@
 package com.example.trialsinthewild;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Activity Launched
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.home);
+        }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            case R.id.owner:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new OwnerFragment()).commit();
+                break;
+            case R.id.experimenter:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ExperimenterFragment()).commit();
+                break;
+            case R.id.labs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LabsFragment()).commit();
+                break;
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
+    }
     @Override
     protected void onStart() {
         // Activity Started (Happens after onRestart)
---
+++ TrialsInTheWild/app/src/main/java/com/example/trialsinthewild/OwnerFragment.java
@@ -0,0 +1,20 @@
+package com.example.trialsinthewild;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class OwnerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_owner, container, false);
+    }
+}
