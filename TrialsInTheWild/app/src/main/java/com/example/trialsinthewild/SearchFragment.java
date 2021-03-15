181f1fdb8301304fbf901a7b0384f37d54429301
JiaYiChe <59781205+JiaYiChe@users.noreply.github.com>
2021-03-15 12:00:14 -0500

    Updated navigation

--- TrialsInTheWild/app/src/main/java/com/example/trialsinthewild/SearchFragment.java
+++ TrialsInTheWild/app/src/main/java/com/example/trialsinthewild/SearchFragment.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class LabsFragment extends Fragment {
+public class SearchFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_labs, container, false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 }
